2119280d7f7f9b63649e94755190c63111604ec5
Raju Sen <rajugbp2006@gmail.com>
2026-08-22 13:20:22 +0530

    auth fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,12 +30,10 @@ const generateTokenAndSetCookie = (res, userId) => {
     }
   );
 
-  const isProduction = process.env.NODE_ENV === "production";
-
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   });
